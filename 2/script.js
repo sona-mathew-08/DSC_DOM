6b1966421a21eb9780f14e86692d4bdf3b6a974d
Sona Mathew <77332715+sona-mathew-08@users.noreply.github.com>
2022-10-26 14:55:09 +0530

    initial commit

---
+++ 2/script.js
@@ -0,0 +1,22 @@
+//Uncomment only when told
+// const Button = document.getElementById("btn");
+// Button.addEventListener("click", handleClick)
+// Button.addEventListener("click", handleHover)
+
+
+// function handleClick() {
+//     const Head = document.getElementById("head");
+//     Head.innerHTML = "Hello World"
+// }
+
+// function handleHover() {
+//     const Second = document.getElementById("second")
+//     Second.innerHTML = "Bye Bye"
+// }
+
+// const inputInfo = document.getElementById("input-text");
+// inputInfo.addEventListener("keyup", handleClick)
+
+// function handleClick(e) {
+//     console.log(e.target.value)
+// }
